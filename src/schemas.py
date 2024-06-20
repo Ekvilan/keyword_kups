e80ce6189ef885f78e57b81649b8d44e18b0909e
Ivan Akimov <128196289+Ekvilan@users.noreply.github.com>
2024-06-20 18:13:39 +1100

    Update schemas.py

--- src/schemas.py
+++ src/schemas.py
@@ -5,5 +5,5 @@ from mail_in_word.connection import export_to_word
 from queries.core import select_dataBase
 data = select_dataBase()
 falename = export_to_word(data)
-#send_word_email(falename, username, password, sender_emails)
+send_word_email(falename, username, password, sender_email)
 print("файл отправлен")
